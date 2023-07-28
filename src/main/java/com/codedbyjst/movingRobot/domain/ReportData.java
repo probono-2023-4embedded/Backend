a865dae4ab70e49eb1ab016dd088beb2f7de200f
jst0951 <jst0951@gmail.com>
2023-07-28 10:56:09 +0900

    bugfix: domain단에서도 Long의 @NotBlank를 @Column(nullable = false)로 변경

--- src/main/java/com/codedbyjst/movingRobot/domain/ReportData.java
+++ src/main/java/com/codedbyjst/movingRobot/domain/ReportData.java
@@ -2,6 +2,7 @@ package com.codedbyjst.movingRobot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,7 +18,7 @@ public class ReportData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reportId;
 
-    @NotBlank
+    @Column(nullable = false)
     private Long userId;
 
     @CreationTimestamp
